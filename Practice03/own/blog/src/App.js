22a81007f4e52ee1e5e914733a4360edb59b4aa5
tom <qoo1593572468@gmail.com>
2019-03-23 22:58:31 +0800

    v1

---
+++ Practice03/own/blog/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src="https://www.ecampusnews.com/files/2016/01/blogs.jpg" />
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
